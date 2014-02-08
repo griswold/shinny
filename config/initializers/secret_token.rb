78ccf01884f20bb19a9a118f25eabfacd7ef08ab
Jim Griswold <jim.griswold@gmail.com>
2014-02-08 16:45:28 -0500

    Rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shinny::Application.config.secret_key_base = 'c88f0b77862909b31734af688c018224f21491a8f6a0ec39154d6dfe1539bfa828288c1d46556662e1afe36482495ce0696f04ce858220fc1e3701c3420f494f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shinny::Application.config.session_store :cookie_store, key: '_shinny_session'
