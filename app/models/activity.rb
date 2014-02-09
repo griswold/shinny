43e19cfc6c0c683c946423bcee7231acb6ad016a
Jim Griswold <jim.griswold@gmail.com>
2014-02-09 14:42:13 -0500

    processor

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,11 @@
 class Activity < ActiveRecord::Base
+
+  def self.recognize(string)
+    arg_words = string.squish.downcase.split(/\s+/)
+    all.map do |activity|
+      words = activity.name.squish.downcase.split(/\s+/)
+      [activity, (words & arg_words).size]
+    end.sort_by(&:last).first
+  end
+
 end
---
+++ app/models/schedule_entry_processor.rb
@@ -0,0 +1,7 @@
+class ScheduleEntryProcessor
+
+  def process(rink, entry)
+    
+  end
+
+end
