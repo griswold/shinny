9b88e58e5d99509e33cf9df373ff432f467a2fb3
Jim Griswold <jim.griswold@gmail.com>
2014-02-09 16:52:10 -0500

    Show all inputs

--- app/controllers/scheduled_activities_controller.rb
+++ app/controllers/scheduled_activities_controller.rb
@@ -9,12 +9,21 @@ class ScheduledActivitiesController < ApplicationController
     start_time = Time.zone.local(@date.year, @date.month, @date.day, @time.hour, @time.min, @time.sec)
     end_time = start_time + 24.hours
 
+    @gender = params[:gender] if params[:gender].present?
+    @age = params[:age].to_i if params[:age].present?
+
     @activity = params[:activity_id].present? ? Activity.find(params[:activity_id]) : Activity.default
     @activities = Activity.all
+
     @scheduled_activities = ScheduledActivity.where(end_time: start_time..end_time,
                                                     activity: @activity)
                                              .order("start_time asc")
                                              .limit(20)
                                              .includes(:rink, :activity)
+    @scheduled_activities = @scheduled_activities.where("gender = ? or gender is null", @gender) if @gender
+    if @age
+      @scheduled_activities = @scheduled_activities.where("start_age is null or start_age <= ?", @age)
+      @scheduled_activities = @scheduled_activities.where("end_age is null or end_age >= ?", @age)
+    end
   end
 end
