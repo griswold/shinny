b495feb394e3ff2d5a161a8a740c2e8ba5d82495
Jim Griswold <jim.griswold@gmail.com>
2014-02-09 14:04:37 -0500

    comment

--- app/models/schedule_entry.rb
+++ app/models/schedule_entry.rb
@@ -1,3 +1,5 @@
+# loosely structured precursor to ScheduledActivity that allows
+# cleaner separation of scraping schedules and the more normalized model
 class ScheduleEntry < Struct.new(:label, :start_time, :end_time)
   def to_s
     %Q(#{label.inspect}: #{start_time.strftime("%Y-%m-%d %H:%M:%S")} - #{end_time.strftime("%Y-%m-%d %H:%M:%S")})
