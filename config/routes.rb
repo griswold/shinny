9bdcb7b8334a07cda29eef3b0ad4b7a2bbc47890
Jim Griswold <jim.griswold@gmail.com>
2014-02-09 16:28:30 -0500

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Shinny::Application.routes.draw do
-  root to: "rinks#index"
+  root to: "scheduled_activities#index"
   resources :rinks, only: %w( index )
   resources :scheduled_activities, only: %w( index )
 end
