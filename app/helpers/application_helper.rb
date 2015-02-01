58a79e5b30b8d945f462ef2bed0cb6eb5deb5672
Jim Griswold <jim.griswold@gmail.com>
2015-01-31 22:52:22 -0500

    Zone-aware date for activity list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def format_activity_time(start_time, end_time)
     time_of_day_format = "%-I:%M%P"
-    start_format = [("%b %-d" unless start_time.to_date == Date.today), time_of_day_format].compact.join(" ")
+    start_format = [("%b %-d" unless start_time.to_date == Date.current), time_of_day_format].compact.join(" ")
     [start_time.strftime(start_format), "-", end_time.strftime(time_of_day_format)].join(" ")
   end
 
