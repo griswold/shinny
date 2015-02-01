8fc05bcebc27374af354924687dbd68417c777d0
Jim Griswold <jim.griswold@gmail.com>
2015-01-31 20:11:35 -0500

    Basic rink page

--- app/controllers/rinks_controller.rb
+++ app/controllers/rinks_controller.rb
@@ -2,4 +2,8 @@ class RinksController < ApplicationController
   def index
     @rinks = Rink.ordered
   end
+
+  def show
+    @rink = Rink.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Shinny::Application.routes.draw do
   root to: "scheduled_activities#index"
-  resources :rinks, only: %w( index )
+  resources :rinks, only: %w( index show )
   resources :scheduled_activities, only: %w( index )
 end
