67df01f30d34bc7f60419fb9e6fdb4d899ac113d
Jim Griswold <jim.griswold@gmail.com>
2015-01-31 19:30:06 -0500

    Search object

--- app/controllers/scheduled_activities_controller.rb
+++ app/controllers/scheduled_activities_controller.rb
@@ -3,24 +3,18 @@ class ScheduledActivitiesController < ApplicationController
   before_filter :set_location
 
   def index
-    start_time = params[:date].present? ? Time.zone.parse(params[:date]) : Time.zone.now
-    end_time = start_time.end_of_day
-
-    @date = start_time.to_date
-    @gender = params[:gender] if params[:gender].present?
-    @age = params[:age].to_i if params[:age].present?
-    @distance = params[:distance].to_i if params[:distance].present?
-    @activity = params[:activity_id].present? ? Activity.find(params[:activity_id]) : Activity.default
     @activities = Activity.all
 
-    @scheduled_activities = ScheduledActivity.search start_time: start_time,
-                                                     end_time: end_time,
-                                                     activity: @activity,
-                                                     limit: 20,
-                                                     location: @location,
-                                                     age: @age,
-                                                     gender: @gender,
-                                                     distance: @distance
+    @search = Search.new(params[:search])
+
+    @scheduled_activities = ScheduledActivity.search start_time: @search.start_time,
+                                                     end_time: @search.end_time,
+                                                     activity: @search.activity,
+                                                     limit: @search.limit,
+                                                     location: @search.location,
+                                                     age: @search.age,
+                                                     gender: @search.gender,
+                                                     distance: @search.distance
 
     respond_to do |format|
       format.html
---
+++ app/models/search.rb
@@ -0,0 +1,35 @@
+class Search
+  include ActiveModel::Model
+  attr_accessor :activity_id, :age, :distance, :gender, :location, :start
+
+  def initialize(params={})
+    super
+    cast_int(:age, :distance)
+  end
+
+  def start_time
+    start.present? ? Time.zone.parse(start) : Time.zone.now
+  end
+
+  def end_time
+    start_time.end_of_day
+  end
+
+  def activity
+    activity_id.present? ? Activity.find(activity_id) : Activity.default
+  end
+
+  def limit
+    20
+  end
+
+  private
+
+  def cast_int(*attr_names)
+    attr_names.each do |name|
+      val = send(name).present? ? send(name).to_i : nil
+      send("#{name}=", val)
+    end
+  end
+
+end
